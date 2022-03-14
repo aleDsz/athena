9238fcadc97dd24148714f6ddf1cfb0bfeb770e1
Alexandre de Souza <alexandre@aledsz.com.br>
2022-03-14 02:29:03 -0300

    feat: add files source and header

---
+++ src/scripts-cli/files.cpp
@@ -0,0 +1,144 @@
+/*
+
+ $$$$$$\    $$\     $$\
+$$  __$$\   $$ |    $$ |
+$$ /  $$ |$$$$$$\   $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\
+$$$$$$$$ |\_$$  _|  $$  __$$\ $$  __$$\ $$  __$$\  \____$$\
+$$  __$$ |  $$ |    $$ |  $$ |$$$$$$$$ |$$ |  $$ | $$$$$$$ |
+$$ |  $$ |  $$ |$$\ $$ |  $$ |$$   ____|$$ |  $$ |$$  __$$ |
+$$ |  $$ |  \$$$$  |$$ |  $$ |\$$$$$$$\ $$ |  $$ |\$$$$$$$ |
+\__|  \__|   \____/ \__|  \__| \_______|\__|  \__| \_______|
+
+ */
+
+#include <dirent.h>
+
+#include <iostream>
+
+#include "files.hpp"
+#include "utils.hpp"
+
+std::vector<std::string> listDirectoriesFrom(std::string baseDirectory);
+
+ /*
+  * List contents from given directory.
+  *
+  * \param directory The given directory to list contents
+  * \param contentType The content type to be filtered
+  * \return The content list from given directory
+  */
+static std::vector<std::string> listDirectoryContent(std::string directory, int contentType)
+{
+	std::vector<std::string> contentList;
+	DIR* dir;
+	struct dirent* ent;
+
+	contentList.clear();
+
+	if ((dir = opendir(directory.c_str())) != NULL) {
+		while ((ent = readdir(dir)) != NULL) {
+			std::string content = std::string(ent->d_name);
+
+			if (ent->d_type == contentType)
+			{
+				contentList.push_back(content);
+			}
+			else if (contentType == DT_UNKNOWN && ent->d_type != DT_DIR)
+			{
+				contentList.push_back(content);
+			}
+		}
+
+		closedir(dir);
+	}
+
+	return contentList;
+}
+
+/*
+ * List files from given directory.
+ *
+ * \param directory The given directory to list files
+ * \return The file list from given directory with Athena's extension
+ */
+static std::vector<std::string> listFilesFrom(std::string directory)
+{
+	std::vector<std::string> tempFileList = listDirectoryContent(directory, DT_UNKNOWN);
+	std::vector<std::string> fileList;
+
+	for (const auto& content : tempFileList) {
+		if (content.length() > 5) {
+			std::string ext = content.substr(content.size() - 4);
+
+			if (ext == ATHENA_SCRIPT_EXTENSION) {
+				fileList.push_back(content);
+			}
+		}
+	}
+
+	return fileList;
+}
+
+/*
+ * List directories from given directory.
+ *
+ * \param baseDirectory The given directory to list directories
+ * \param sourceList The given directory to list directories
+ * \param directoryList The given directory to list directories
+ */
+static void listDirectoriesFrom(std::string baseDirectory, std::vector<std::string> sourceList, std::vector<std::string>* directoryList)
+{
+	if (directoryList->empty()) {
+		sourceList = listDirectoryContent(baseDirectory, DT_DIR);
+	}
+
+	for (std::string directory : sourceList) {
+		if (directory.length() > 2)
+		{
+			// std::shared_ptr<std::string> c_directory(directory);
+
+			directoryList->push_back(directory);
+			std::string path = getFullPath(baseDirectory, directory);
+			std::vector<std::string> directories = listDirectoriesFrom(baseDirectory);
+
+			listDirectoriesFrom(path, directories, directoryList);
+		}
+	}
+}
+
+/*
+ * List directories from given directory.
+ *
+ * \param baseDirectory The given directory to list directories
+ * \return The directory list from given directory
+ */
+static std::vector<std::string> listDirectoriesFrom(std::string baseDirectory)
+{
+	std::vector<std::string> sourceList = listDirectoryContent(baseDirectory, DT_DIR);
+	std::vector<std::string>* directoryList;
+
+	listDirectoriesFrom(baseDirectory, sourceList, directoryList);
+
+	return *directoryList;
+}
+
+/*
+ * List all file tree under the given base directory.
+ *
+ * \param baseDirectory The base directory to discover the file tree
+ * \return A map with path as key and vector of string of files
+ */
+std::map<std::string, std::vector<std::string>> listFileTreeFrom(std::string baseDirectory)
+{
+	std::map<std::string, std::vector<std::string>> fileTree;
+	std::vector<std::string> directories = listDirectoriesFrom(baseDirectory);
+
+	for (const auto& directory : directories) {
+		std::vector<std::string> fileList = listFilesFrom(directory);
+		std::pair<std::string, std::vector<std::string>> directoryFileListPair(directory, fileList);
+
+		fileTree.insert(directoryFileListPair);
+	}
+
+	return fileTree;
+}
---
+++ src/scripts-cli/files.hpp
@@ -0,0 +1,25 @@
+/*
+
+ $$$$$$\    $$\     $$\
+$$  __$$\   $$ |    $$ |
+$$ /  $$ |$$$$$$\   $$$$$$$\   $$$$$$\  $$$$$$$\   $$$$$$\
+$$$$$$$$ |\_$$  _|  $$  __$$\ $$  __$$\ $$  __$$\  \____$$\
+$$  __$$ |  $$ |    $$ |  $$ |$$$$$$$$ |$$ |  $$ | $$$$$$$ |
+$$ |  $$ |  $$ |$$\ $$ |  $$ |$$   ____|$$ |  $$ |$$  __$$ |
+$$ |  $$ |  \$$$$  |$$ |  $$ |\$$$$$$$\ $$ |  $$ |\$$$$$$$ |
+\__|  \__|   \____/ \__|  \__| \_______|\__|  \__| \_______|
+
+ */
+
+#ifndef __ATHENA_FILES_H__
+#define __ATHENA_FILES_H__
+
+#include <string>
+#include <map>
+#include <vector>
+
+#define ATHENA_SCRIPT_EXTENSION "atxt"
+
+std::map<std::string, std::vector<std::string>> listFileTreeFrom(std::string fullPath);
+
+#endif // __ATHENA_FILES_H__
